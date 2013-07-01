f4b75cff9848bdc62cb78b30224878c202fa6411
Pawel Piotrowski <pawel@koffeecup.net>
2013-07-01 02:18:25 +0100

    crafty init

--- js/si-functions-plain.js
+++ js/si-functions-plain.js
@@ -1,6 +1,6 @@
 /** DOMReady v2.0.1 - MIT license - https://github.com/freelancephp/DOMReady */
 (function(a){a.DOMReady=function(){var b=[],c=false,d=null,e=function(a,b){try{a.apply(this,b||[])}catch(c){if(d)d.call(this,c)}},f=function(){c=true;for(var a=0;a<b.length;a++)e(b[a].fn,b[a].args||[]);b=[]};this.setOnError=function(a){d=a;return this};this.add=function(a,d){if(c){e(a,d)}else{b[b.length]={fn:a,args:d}}return this};if(a.addEventListener){a.document.addEventListener("DOMContentLoaded",function(){f()},false)}else{(function(){if(!a.document.uniqueID&&a.document.expando)return;var b=a.document.createElement("document:ready");try{b.doScroll("left");f()}catch(c){setTimeout(arguments.callee,0)}})()}return this}()})(window);
-
+/*
 function invade() {
 	var invRows = document.querySelectorAll('.invaders-row');
 	for(var i = 0; i < invRows.length; i++) {
@@ -13,4 +13,14 @@ function invade() {
 		} while (j < 10);
 	}
 }
-DOMReady.add(function(){ invade() });
+*/
+SI = {
+  // Initialize and start our game
+  start: function() {
+    // Start crafty and set a background color so that we can see it's working
+    Crafty.init(480, 320);
+    Crafty.background('green');
+  }
+}
+
+DOMReady.add(function(){ SI.start() });
